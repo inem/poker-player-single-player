b0e150257afe4fed4142cd35179e599afc9a05fc
Ivan Nemytchenko <nemytchenko@gmail.com>
2015-06-27 13:38:00 +0200

    ...

--- player.rb
+++ player.rb
@@ -1,20 +1,41 @@
 require 'json'
 class Player
-  VERSION = "rand(999)"
+  VERSION = "smart boy"
 
   def bet_request(game_state)
-    # raise game_state.inspect
-    # data = JSON.load(game_state).first
-    # puts data.inspect
-    # require 'pry'; binding.pry
+    hand = game_state["players"].select{|a|a["name"]  == "Single Player"}.first["hole_cards"]
+    values = game_state["players"].select{|a|a["name"]  == "Single Player"}.first["hole_cards"].map{|a| a["rank"]}
 
-    # data =
-    rand(999)
+    a = 0
+    if values.size == 2
+      c1 = values.first
+      c2 = values.last
+
+      if c1 == c2
+        a = a + 2
+      end
+
+      a = a + 1 if["A", "K", "Q"].include?(c1)
+      a = a + 1 if["A", "K", "Q"].include?(c2)
+
+      if a == 0
+        0
+      elsif a == 1
+        rand(300)
+      else
+        600
+      end
+    else
+      rand(999)
+    end
   end
 
   def showdown(game_state)
-
+    puts game_stats
   end
+
+  private
+
 end
 
 
