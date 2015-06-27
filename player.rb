c004d0066f712b90f47667e656bd38a67f047922
Ivan Nemytchenko <nemytchenko@gmail.com>
2015-06-27 15:04:21 +0200

    ...

--- player.rb
+++ player.rb
@@ -8,11 +8,13 @@ class Player
     current_buy_in = game_state["current_buy_in"]
     min_raise = game_state["minimum_raise"]
 
+    same_amount_on_table = game_state["players"].select{|a|a["name"] != "Single Player"}.uniq
+
     hand = game_state["players"].select{|a|a["name"]  == "Single Player"}.first["hole_cards"]
     values = game_state["players"].select{|a|a["name"]  == "Single Player"}.first["hole_cards"].map{|a| a["rank"]}
 
     a = 0
-    if values.size == 2
+    # if values.size == 2
       c1 = values.first
       c2 = values.last
 
@@ -24,11 +26,15 @@ class Player
       elsif a == 1
         current_buy_in
       else
-        current_buy_in + min_raise
+        if same_amount_on_table.size == 1
+          current_buy_in
+        else
+          current_buy_in + min_raise
+        end
       end
-    else
-      rand(min_raise*3) + 2*min_raise
-    end
+    # else
+    #   rand(min_raise*3) + 2*min_raise
+    # end
   end
 
   def showdown(game_state)
